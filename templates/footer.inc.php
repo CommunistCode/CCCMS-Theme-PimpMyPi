90a06f4988ad122e7941426c16863f342ddb4f18
Jack Mitchell <jack@communistcode.co.uk>
2012-02-02 16:07:33 +0000

    misc improvements

--- templates/footer.inc.php
+++ templates/footer.inc.php
@@ -1,1 +1,17 @@
 <a href='mailto:jack@embed.me.uk'>&copy; Jack Mitchell</a>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-28883319-1']);
+  _gaq.push(['_setDomainName', 'none']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
